1918d80df5c8fa242c4ebb0ebe6b227af995c9e5
Walter Dal Mut <walter.dalmut@gmail.com>
2018-10-31 11:23:28 +0100

    New users

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const { ifElse, bind, compose, isNil, flip, prop } = require('ramda')
 
 const users = {
-  "da39a3ee5e6b4b0d3255bfef95601890afd80709": {id: 1, firstname: "John", lastname: "Smith", role: "ROLE_USER"},
-  "10a34637ad661d98ba3344717656fcc76209c2f8": {id: 2, firstname: "John", lastname: "Doe", role: "ROLE_USER"},
-  "3e6c06b1a28a035e21aa0a736ef80afadc43122c": {id: 3, firstname: "Michael", lastname: "Corleone", role: "ROLE_ADMIN"}
+  "da39a3ee5e6b4b0d3255bfef95601890afd80709": {id: 1, firstname: "John", lastname: "Smith", role: "ROLE_USER", gender: 'M', username: 'john.smith@example.com'},
+  "10a34637ad661d98ba3344717656fcc76209c2f8": {id: 2, firstname: "Mary", lastname: "Santoro", role: "ROLE_USER", gender: 'F', username: 'mary.santoro@example.com'},
+  "3e6c06b1a28a035e21aa0a736ef80afadc43122c": {id: 3, firstname: "Michael", lastname: "Corleone", role: "ROLE_ADMIN", gender: 'M', username: 'michael.corleone@example.com'}
 }
 
 module.exports = ifElse(
